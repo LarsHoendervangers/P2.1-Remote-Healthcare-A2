dcd25fbdc5e963f388409ec75e9318a00b214a2a
Lars <larslego@outlook.com>
2021-09-01 15:19:09 +0200

    Added: Listeners for heartbeat, rpm and speed. Added: DataSimulator to test the console. Edit: Simplified ConsoleWindow.

---
+++ RemoteHealthcare/Simulator/DataSimulator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RemoteHealthcare.UI.Interfaces;
+
+namespace RemoteHealthcare.Simulator
+{
+    class DataSimulator
+    {
+        private IHeartbeatListener heartBeatListener;
+        private ISpeedListener speedListener;
+        private IRPMListener rpmListener;
+
+        private Random random = new Random();
+
+        public DataSimulator()
+        {
+
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                if (this.heartBeatListener != null)
+                {
+                    this.heartBeatListener.OnHeartBeatChanged(this.random.Next(200));
+                }
+                
+                if (this.speedListener != null)
+                {
+                    this.speedListener.OnSpeedChanged(this.random.Next(60));
+                }
+
+                if (this.rpmListener != null)
+                {
+                    this.rpmListener.OnRPMChanged(this.random.Next(1000));
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+        public void SetHeartBeatListener(IHeartbeatListener listener)
+        {
+            this.heartBeatListener = listener;
+        }
+
+        public void SetSpeedListener(ISpeedListener listener)
+        {
+            this.speedListener = listener;
+        }
+
+        public void SetRPMListener(IRPMListener listener)
+        {
+            this.rpmListener = listener;
+        }
+    }
+}
---
+++ RemoteHealthcare/UI/ConsoleWindow.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using RemoteHealthcare.UI.Interfaces;
+
+namespace RemoteHealthcare.UI
+{
+    enum Line : int
+    {
+        BrandLine = 0,
+        RPMLine = 2,
+        SpeedLine = 3,
+        HeartLine = 4,
+        InputLine = 10,
+        OutputLine = 12
+    }
+
+    class ConsoleWindow : ISpeedListener, IHeartbeatListener, IRPMListener
+    {
+        private readonly string enterCommandMsg = "Enter a command: ";
+        public ConsoleWindow()
+        {
+            Console.CursorVisible = false;
+        }
+
+        public void PrintData()
+        {
+            PrintBikeData();
+            DrawUI();
+        }
+
+        private void DrawUI()
+        {
+            Console.SetCursorPosition(0, (int) Line.BrandLine);
+            Console.WriteLine("Remote Healthcare by A2");
+
+            // Draw a divider line between the data and the input line.
+            for (int x = 0; x < Console.BufferWidth; x++)
+            {
+                Console.SetCursorPosition(x, (int) Line.InputLine - 1);
+                Console.Write("-");
+            }
+
+            // Clear the input line and reset the cursor. After that print a msg.
+            // USER INPUT IS CURRENTLY DISABLED
+            //Console.SetCursorPosition(0, (int) Line.InputLine);
+            //Console.Write(this.enterCommandMsg);
+        }
+
+        private void PrintBikeData()
+        {
+            Console.SetCursorPosition(0, (int) Line.RPMLine);
+            Console.Write("Power: 5000 Watt");
+            Console.SetCursorPosition(0, (int) Line.HeartLine + 1);
+        }
+
+        private void ClearLine(int line)
+        {
+            ClearLine(0, line);
+        }
+
+        private void ClearLine(int fromX, int line)
+        {
+            for (int x = fromX; x < Console.BufferWidth; x++)
+            {
+                Console.SetCursorPosition(x, line);
+                Console.Write(" ");
+            }
+            Console.SetCursorPosition(0, line);
+        }
+
+        public void OnSpeedChanged(int speed)
+        {
+            //ClearLine(7, (int) Line.SpeedLine);
+            Console.SetCursorPosition(0, (int) Line.SpeedLine);
+            Console.Write("Speed: {0} km/h     ", speed);
+        }
+
+        public void OnHeartBeatChanged(int heartBeat)
+        {
+            //ClearLine(11, (int) Line.HeartLine);
+            Console.SetCursorPosition(0, (int) Line.HeartLine);
+            Console.Write("Heartbeat: {0} BPM     ", heartBeat);
+        }
+
+        public void OnRPMChanged(int rpm)
+        {
+            //ClearLine(5, (int)Line.RPMLine);
+            Console.SetCursorPosition(0, (int) Line.RPMLine);
+            Console.Write("RPM: {0}          ", rpm);
+        }
+    }
+}
