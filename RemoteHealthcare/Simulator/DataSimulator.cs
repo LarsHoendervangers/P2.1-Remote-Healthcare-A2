6aa6703655b0a3a7f8d0e9c2ca895aeddf43793e
TONI330 <t.j.tleenders@gmail.com>
2021-09-02 14:07:07 +0200

    added files

---
+++ RemoteHealthcare/Simulator/DataSimulator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RemoteHealthcare.UI.Interfaces;
+
+namespace RemoteHealthcare.Simulator
+{
+    class DataSimulator
+    {
+        private IHeartbeatListener heartBeatListener;
+        private ISpeedListener speedListener;
+        private IRPMListener rpmListener;
+        private IResistanceListener resistanceListener;
+        private IDistanceListener distanceListener;
+
+        private Random random = new Random();
+
+        public DataSimulator()
+        {
+
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                if (this.heartBeatListener != null)
+                {
+                    this.heartBeatListener.OnHeartBeatChanged(this.random.Next(200));
+                }
+                
+                if (this.speedListener != null)
+                {
+                    this.speedListener.OnSpeedChanged(this.random.Next(60));
+                }
+
+                if (this.rpmListener != null)
+                {
+                    this.rpmListener.OnRPMChanged(this.random.Next(1000));
+                }
+
+                if (this.resistanceListener != null)
+                {
+                    this.resistanceListener.OnResistanceChanged(this.random.Next(100));
+                }
+
+                if (this.distanceListener != null)
+                {
+                    this.distanceListener.OnDistanceChanged(i);
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+        public void SetHeartBeatListener(IHeartbeatListener listener)
+        {
+            this.heartBeatListener = listener;
+        }
+
+        public void SetSpeedListener(ISpeedListener listener)
+        {
+            this.speedListener = listener;
+        }
+
+        public void SetRPMListener(IRPMListener listener)
+        {
+            this.rpmListener = listener;
+        }
+
+        public void SetResitanceListener(IResistanceListener listener)
+        {
+            this.resistanceListener = listener;
+        }
+
+        public void SetDistanceListener(IDistanceListener listener)
+        {
+            this.distanceListener = listener;
+        }
+    }
+}
---
+++ RemoteHealthcare/UI/Interfaces/IDistanceListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteHealthcare.UI.Interfaces
+{
+    interface IDistanceListener
+    {
+        void OnDistanceChanged(int distance);
+    }
+}
