545cfb41c9eb5df35b6d4c8e1a15e683eed105d6
Lars <larslego@outlook.com>
2021-09-08 13:51:40 +0200

    Added: ClearLine and two line drawing methods in GUITools.

--- RemoteHealthcare/Program.cs
+++ RemoteHealthcare/Program.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using RemoteHealthcare.Tools;
+using System;
 
 namespace RemoteHealthcare
 {
     class Program
-
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DrawLayout();
+        }
+
+        private static void DrawLayout()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("Remote Healthcare by A2");
+            GUITools.DrawHorizontalLine(1, 0, Console.BufferWidth);
         }
     }
 }
