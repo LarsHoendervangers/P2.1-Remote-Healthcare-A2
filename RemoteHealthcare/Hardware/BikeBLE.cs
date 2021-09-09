6bbfad3c138ee028869be694b93a3cd310cf4e91
Jesse <jm.krijgsman@student.avans.nl>
2021-09-09 10:10:17 +0200

    Cleaned method from BikeBLE

--- RemoteHealthcare/Hardware/BikeBLE.cs
+++ RemoteHealthcare/Hardware/BikeBLE.cs
@@ -13,8 +13,9 @@ namespace RemoteHealthcare.Hardware
     class BikeBLE : BLE
     {
 
-        public event EventHandler<Byte[]> onBikeData;
+        public event EventHandler<Byte[]> OnBikeData;
         private readonly PhysicalDevice device;
+
         private readonly string BikeName;
         int errorcode = 1;
         private int connectionAttempts = 0;
@@ -26,14 +27,18 @@ namespace RemoteHealthcare.Hardware
             // Waiting beforeinitializing
             Thread.Sleep(1000);
 
-            // ignore async problem, function can continue
             Console.WriteLine("Initializing...");
+
+            // ignore async problem, function can continue 
             Initialize();
         }
 
+        /*
+         * Starts the connection to the bike and subscribes to the correct value's
+         */ 
         private async Task Initialize()
         {
-            // Open the correct device
+            // Open the correct device, when connection failed it retries to connect
             while(this.errorcode != 0)
             {
                 this.connectionAttempts += 1;
@@ -49,18 +54,18 @@ namespace RemoteHealthcare.Hardware
             this.errorcode = await SubscribeToCharacteristic("6e40fec2-b5a3-f393-e0a9-e50e24dcca9e");
         }
 
-        private bool checkReceivedBike()
-        {
-            return true; //TODO not implemented
-        }
-
+        /*
+         * Event method that is called when the BLE receives data.
+         * The method checks if the data is correct and sends it to the device class for decoding.
+         */
         private void onBikeMovement(object sender, BLESubscriptionValueChangedEventArgs e)
         {
 
+            // check if the date was received correct, by checking the checksum
             if (ProtocolConverter.ChecksumContol(e.Data))
-            {
-                this.onBikeData?.Invoke(this, e.Data);
-            }
+                //Invoking the the event to sent the data to the Device class
+                this.OnBikeData?.Invoke(this, e.Data);
+            
         }
     }
 }
