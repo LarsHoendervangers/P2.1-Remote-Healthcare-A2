30ba9a7b733214f019efbfe5313bdc19d8e424d1
luuk <lah.vanberkel@student.avans.nl>
2021-09-09 09:15:20 +0200

    Auto stash before merge of "feature-gui" and "origin/feature-gui"
    Start perlin noise generation

--- RemoteHealthcare/Software/SimulatedDevice.cs
+++ RemoteHealthcare/Software/SimulatedDevice.cs
@@ -7,13 +7,34 @@ using System.Threading.Tasks;
 
 namespace RemoteHealthcare.Software
 {
-    class SimulatedDevice
+    class SimulatedDevice : Device
     {
         private SimDataGenerator Generator{ get; set; }
         private SimGUI User{ get; set; }
 
         public SimulatedDevice()
         {
+        }
+
+        public override event EventHandler<double> onSpeed;
+        public override event EventHandler<int> onRPM;
+        public override event EventHandler<int> onHeartrate;
+        public override event EventHandler<double> onDistance;
+        public override event EventHandler<int> onElapsedTime;
+
+
+
+
+
+
+        public override void OnHeartBeatReceived(object sender, byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnBikeReceived(object sender, byte[] data)
+        {
+            throw new NotImplementedException();
         }
     }
 }
