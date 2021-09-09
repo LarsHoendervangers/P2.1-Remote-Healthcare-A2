910582fa3769265799f85cc2b692dd32d7290f60
Lars <larslego@outlook.com>
2021-09-09 13:35:18 +0200

    Edit: Fixed resistance not correctly setting.

--- RemoteHealthcare/Graphics/DataGUI.cs
+++ RemoteHealthcare/Graphics/DataGUI.cs
@@ -1,21 +1,15 @@
-﻿using RemoteHealthcare.Hardware;
-using RemoteHealthcare.Software;
+﻿using RemoteHealthcare.Software;
 using RemoteHealthcare.Tools;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace RemoteHealthcare.Graphics
 {
     class DataGUI
     {
         private static string Title = "Remote Healthcare by A2";
-        private static bool IsTyping = false;
 
+        // These define the lines in the console.
         private static int RPM_Line = 2;
         private static int Speed_Line = 3;
         private static int Heart_Line = 4;
@@ -25,7 +19,9 @@ namespace RemoteHealthcare.Graphics
         private static int CurrentPower_Line = 8;
         private static int Input_Line = 9;
 
+        // This is the 'index' of the list of devices in the console.
         private static int CurrentDeviceLine = 2;
+        // This keeps track of the longest name so that the vertical devider can be drawn on the correct position.
         private static int LongestDeviceName = 0;
 
         private Device device = new PhysicalDevice("Tacx Flux 00457", "Decathlon Dual HR");
@@ -47,9 +43,12 @@ namespace RemoteHealthcare.Graphics
 
         static void Main(string[] args)
         {
-            new DataGUI().Start();
+            DataGUI gui = new DataGUI();
+            gui.Start();
         }
 
+        // This method starts the user input listener.
+        // When spacebar is pressed, the input will be sent automatically.
         private void Start()
         {
             string resistance = "Resistance: ";
@@ -59,7 +58,8 @@ namespace RemoteHealthcare.Graphics
 
             Console.SetCursorPosition(0, Input_Line);
             Console.Write(resistance);
-            do
+
+            while (true)
             {
                 char character = Console.ReadKey(true).KeyChar;
                 if (Char.IsNumber(character))
@@ -73,18 +73,16 @@ namespace RemoteHealthcare.Graphics
                     }
                 } else if (Char.IsWhiteSpace(character))
                 {
-                    IsTyping = false;
                     x = resistance.Length;
-                    value = "";
                     GUITools.ClearLine(x, 50, Input_Line);
                     int res;
                     if (int.TryParse(value, out res))
                     {
                         device.OnResistanceCall(this, res);
                     }
-
+                    value = "";
                 }
-            } while (true);
+            }
         }
 
         // Draw the basic layout in the console.
@@ -109,53 +107,39 @@ namespace RemoteHealthcare.Graphics
         // Called by onRPM event.
         private void DrawRPM(object sender, int e)
         {
-            if (!IsTyping) 
-            {
-                Console.SetCursorPosition(0, RPM_Line);
-                Console.WriteLine($"RPM: {e}     ");
-            }
+            Console.SetCursorPosition(0, RPM_Line);
+            Console.WriteLine($"RPM: {e}     ");
         }
 
         // Called by onSpeed event.
         private void DrawSpeed(object sender, double e)
         {
-            if (!IsTyping)
-            {
-                Console.SetCursorPosition(0, Speed_Line);
-                Console.WriteLine($"Speed: {e.ToString("0.##")} KM/H     ");
-            }
+            Console.SetCursorPosition(0, Speed_Line);
+            Console.WriteLine($"Speed: {e.ToString("0.##")} KM/H     ");
         }
 
         // Called by onHeartrate event.
         public void DrawHeartrate(Object sender, int heartrate)
         {
-            if (!IsTyping)
-            {
-                Console.SetCursorPosition(0, Heart_Line);
-                Console.WriteLine($"Heartrate: {heartrate} BPM     ");
-            }
+            Console.SetCursorPosition(0, Heart_Line);
+            Console.WriteLine($"Heartrate: {heartrate} BPM     ");
         }
 
         // Called by onElapsedTime event.
         private void DrawElapsedTime(object sender, double e)
         {
-            if (!IsTyping)
-            {
-                Console.SetCursorPosition(0, Time_Line);
-                Console.WriteLine($"Elapsed time: {e} seconds     ");
-            }
+            Console.SetCursorPosition(0, Time_Line);
+            Console.WriteLine($"Elapsed time: {e} seconds     ");
         }
 
         // Called by onDistance event.
         private void DrawDistance(object sender, double e)
         {
-            if (!IsTyping)
-            {
-                Console.SetCursorPosition(0, Distance_Line);
-                Console.WriteLine($"Distance: {e} m     ");
-            }
+            Console.SetCursorPosition(0, Distance_Line);
+            Console.WriteLine($"Distance: {e} m     ");
         }
 
+        // Called by 
         private void DrawTotalPower(object sender, int e)
         {
             Console.SetCursorPosition(0, TotalPower_Line);
