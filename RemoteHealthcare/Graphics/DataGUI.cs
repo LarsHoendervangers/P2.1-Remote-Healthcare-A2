a981d3f92c5ca95b0d4542e3ff8668948c387fc7
Lars <larslego@outlook.com>
2021-09-09 15:13:35 +0200

    Edit: Converted double to int in DrawDistance event.

--- RemoteHealthcare/Graphics/DataGUI.cs
+++ RemoteHealthcare/Graphics/DataGUI.cs
@@ -151,7 +151,7 @@ namespace RemoteHealthcare.Graphics
         private void DrawDistance(object sender, double e)
         {
             Console.SetCursorPosition(0, Distance_Line);
-            Console.WriteLine($"Distance: {e} m     ");
+            Console.WriteLine($"Distance: {(int) e} m     ");
         }
 
         // Called by onTotalPower event.
