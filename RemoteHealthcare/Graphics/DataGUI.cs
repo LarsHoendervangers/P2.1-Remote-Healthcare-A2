301eb1cb0fceda23e42c4127ecea8cb7d5958002
Lars <larslego@outlook.com>
2021-09-09 14:15:00 +0200

    Fix: Merge error.

--- RemoteHealthcare/Graphics/DataGUI.cs
+++ RemoteHealthcare/Graphics/DataGUI.cs
@@ -2,9 +2,6 @@
 using RemoteHealthcare.Tools;
 using System;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RemoteHealthcare.Graphics
 {
@@ -20,6 +17,7 @@ namespace RemoteHealthcare.Graphics
         private static int Distance_Line = 6;
         private static int TotalPower_Line = 7;
         private static int CurrentPower_Line = 8;
+        private static int Input_Line = 9;
 
         // This is the 'index' of the list of devices in the console.
         private static int CurrentDeviceLine = 2;
@@ -31,8 +29,6 @@ namespace RemoteHealthcare.Graphics
 
         public DataGUI()
         {
-            Device device = new PhysicalDevice("Tacx Flux 00457", "Decathlon Dual HR");
-            //Device device = new SimulatedDevice();
             device.onHeartrate += DrawHeartrate;
             device.onRPM += DrawRPM;
             device.onSpeed += DrawSpeed;
@@ -44,7 +40,7 @@ namespace RemoteHealthcare.Graphics
             PrepareGUI();
         }
 
-        // Main code entry point
+        // Main code entry point.
         static void Main(string[] args)
         {
             DataGUI gui = new DataGUI();
@@ -69,14 +65,14 @@ namespace RemoteHealthcare.Graphics
                 if (Char.IsNumber(character))
                 {
                     if (x < Console.BufferWidth)
-                    { 
+                    {
                         // When an integer is pressed, add it to the 'value' string.
                         Console.SetCursorPosition(x, Input_Line);
                         Console.Write(character);
                         value += character;
                         x++;
                     }
-                } else if (character == (char) 13)
+                } else if (character == (char)13)
                 {
                     // If the enter key is pressed.
                     x = resistance.Length;
@@ -116,7 +112,6 @@ namespace RemoteHealthcare.Graphics
 
             GUITools.DrawBasicLayout("Remote Healthcare by A2");
             Console.CursorVisible = false;
-       
 
             Console.Read();
         }
@@ -156,16 +151,55 @@ namespace RemoteHealthcare.Graphics
             Console.WriteLine($"Distance: {e} m     ");
         }
 
+        // Called by onTotalPower event.
         private void DrawTotalPower(object sender, int e)
         {
             Console.SetCursorPosition(0, TotalPower_Line);
             Console.WriteLine($"Total power: {e} Watt     ");
         }
 
+        // Called by onCurrentPower event.
         private void DrawCurrentPower(object sender, int e)
         {
             Console.SetCursorPosition(0, CurrentPower_Line);
             Console.WriteLine($"Current power: {e} Watt     ");
         }
+
+        // Prints device on the right side of the console.
+        public static void AddDeviceToList(string device)
+        {
+            int x = Console.BufferWidth - 1;
+            int length = device.Length - 1;
+
+            if (length > LongestDeviceName) LongestDeviceName = length;
+
+            foreach (char c in device)
+            {
+                Console.SetCursorPosition(x, CurrentDeviceLine);
+                Console.Write(device[length]);
+                // Check for out of bounds exception.
+                if (x - 1 >= 0) x--;
+
+                // Check for out of bounds exception.
+                if (length - 1 >= 0) length--;
+            }
+            // Increase the current line for the next device.
+            CurrentDeviceLine++;
+        }
+
+        // Print a message to the top of the screen.
+        public static void SetMessage(string msg)
+        {
+            String prefix = "Message: ";
+
+            // Clear the line (just in case)
+            int startX = Title.Length + prefix.Length;
+            GUITools.ClearLine(startX, (Console.BufferWidth - 1) - (LongestDeviceName + 1), 0);
+
+            // Print the new message.
+            Console.SetCursorPosition(Title.Length + prefix.Length, 0);
+            Console.Write(prefix + msg);
+        }
     }
 }
+
