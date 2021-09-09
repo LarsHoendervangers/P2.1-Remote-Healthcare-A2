125927f9d0518a08302ca066909bd42e8c237923
Teun Leenders <t.j.tleenders@gmail.com>
2021-09-09 15:40:02 +0200

    added comments to interface IBLEDevice

--- RemoteHealthcare/Hardware/IBLEDevice.cs
+++ RemoteHealthcare/Hardware/IBLEDevice.cs
@@ -9,15 +9,48 @@ namespace RemoteHealthcare.Hardware
 {
     interface IBLEDevice
     {
+        /// <summary>
+        /// Probably unnessecary
+        /// </summary>
+        /// <param name="deviceName"></param>
+        /// <returns></returns>
         Task<int> OpenDevice(string deviceName);
-        Task<int> SetService(string serviceName);
+        /// <summary>
+        /// Probably unnessecary
+        /// </summary>
+        /// <param name="serviceName"></param>
+        /// <returns></returns>
+        Task<int> SetService(string serviceName);/// <summary>
+        /// Probably unnessecary
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
         Task<int> SubscribeToCharacteristic(string param);
-        //BLESubscriptionValueChangedEventHandler SubscriptionValueChanged;
+        /// <summary>
+        /// Event used for sending data from 3rd layer to 2nd layer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void OnDataReceived(object sender, BLESubscriptionValueChangedEventArgs e);
-
+        /// <summary>
+        /// Error code registered in Device but must be sent to PhysicalDevice
+        /// </summary>
+        /// <param name="errorcode"></param>
         void SetErrorCode(int errorcode);
+        /// <summary>
+        /// Connection attempts registered in Device but must be sent to PhysicalDevice
+        /// </summary>
+        /// <param name="connectionAttempts"></param>
         void SetConnectionAttempts(int connectionAttempts);
+        /// <summary>
+        /// Error code registered in Device but must be sent to PhysicalDevice
+        /// </summary>
+        /// <returns></returns>
         int GetErrorCode();
+        /// <summary>
+        /// Connection attempts registered in Device but must be sent to PhysicalDevice
+        /// </summary>
+        /// <returns></returns>
         int GetConnectionAttempts();
     }
 }
