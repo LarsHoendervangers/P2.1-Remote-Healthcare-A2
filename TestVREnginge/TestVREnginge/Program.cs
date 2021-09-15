2dc3a10513c9a63f6637a513d756b81a94d530c9
luuk <lah.vanberkel@student.avans.nl>
2021-09-15 16:04:16 +0200

    Working of the tunneling

--- TestVREnginge/TestVREnginge/Program.cs
+++ TestVREnginge/TestVREnginge/Program.cs
@@ -12,69 +12,55 @@ namespace TestVREnginge
     {
         static void Main(string[] args)
         {
-            String test = "{\r\n\"id\" : \"session/list\"\r\n}";
-
-            //Sendable byte arrays
-            byte[] payload = Encoding.ASCII.GetBytes(test);
-            byte[] lenght = new byte[4];
-            lenght = BitConverter.GetBytes(test.Length);
-            byte[] final = Combine(lenght, payload);
-
-            //Debug print of data that is send
-            Console.WriteLine(BitConverter.ToString(final));
-
-
             TcpClient client = new TcpClient("145.48.6.10", 6666);
             NetworkStream stream = client.GetStream();
 
-            stream.Write(final, 0, test.Length + 4);
-            stream.Flush();
-
+            string startingCode = "{\r\n\"id\" : \"session/list\"\r\n}";
+            Communication.WriteMessage(stream, startingCode);
 
 
             //Reading
-            Console.WriteLine(  ReadMessage(stream));
+            String jsonString = Communication.ReadMessage(stream);
+            JObject jsonData = (JObject)JsonConvert.DeserializeObject(jsonString);
+
+            List<string> availableComputers = new List<string>();
+            Console.WriteLine("Available Computers: ");
+
+            JArray computers = (JArray)jsonData.GetValue("data");
+            int selection = 1;
+            foreach (JObject objects in computers)
+            {
+                string id = objects.GetValue("id").ToString();
+                JObject clientinfo = (JObject)objects.GetValue("clientinfo");
+                string pcName = clientinfo.GetValue("host").ToString();
+                string pcUser = clientinfo.GetValue("user").ToString();
+                string pcGPU = clientinfo.GetValue("renderer").ToString();
+
+                Console.WriteLine($"Selection: {selection}, ID: {id}, PC: {pcName}, User: {pcUser}, GPU: {pcGPU}");
+
+                
+                availableComputers.Add(id);
+                selection++;
+            }
+
+            int userinput = 0;
+            while (userinput < 1 || userinput > availableComputers.Count)
+            {
+                Console.WriteLine("\nGive a selection number for a tunnel: ");
+                userinput = int.Parse(Console.ReadLine());
+            }
+
+            string requestingCode = "{\r\n\"id\" : \"tunnel/create\",\r\n\"data\" :\r\n	{\r\n\"session\" : \""+ availableComputers[userinput-1]+ "\"\r\n}\r\n}";
+            Communication.WriteMessage(stream, requestingCode);
+
+
+            Console.WriteLine(Communication.ReadMessage(stream));
 
             //Shutting down
             stream.Close();
             client.Close();
         }
 
-
-       
-
-        public static byte[] Combine(byte[] first, byte[] second)
-        {
-            byte[] bytes = new byte[first.Length + second.Length];
-            Buffer.BlockCopy(first, 0, bytes, 0, first.Length);
-            Buffer.BlockCopy(second, 0, bytes, first.Length, second.Length);
-            return bytes;
-        }
-
-        public static string ReadMessage(NetworkStream networkStream)
-        {
-
-            // 4 bytes lenght == 32 bits, always positive unsigned
-            byte[] lenghtArray = new byte[4];
-
-            networkStream.Read(lenghtArray, 0, 4);
-            int lenght = BitConverter.ToInt32(lenghtArray, 0);
-
-            Console.WriteLine(lenght);
-
-            byte[] buffer = new byte[lenght];
-            int totalRead = 0;
-
-            //read bytes until stream indicates there are no more
-            while (totalRead < lenght)
-            {
-                int read = networkStream.Read(buffer, totalRead, buffer.Length - totalRead);
-                totalRead += read;
-                Console.WriteLine("ReadMessage: " + read);
-            } 
-
-            return Encoding.ASCII.GetString(buffer, 0, totalRead);
-        }
 
 
 
