abd6435eedfe9ad5bd06ec6de657657caaa7ffbf
Lars <larslego@outlook.com>
2021-09-16 14:05:21 +0200

    Edit: Fixed bike rotation. Edit: Changed house model to bike. Edit: Added more comments. Edit: Changed Queue to List.

--- TestVREnginge/TestVREnginge/BasicScene.cs
+++ TestVREnginge/TestVREnginge/BasicScene.cs
@@ -8,27 +8,26 @@ namespace TestVREngine
 {
     class BasicScene
     {
-        private Queue<Func<string>> CommandList;
+        private List<Func<string>> CommandList;
         private TunnelHandler Handler;
-        private string uuidRoute;
 
+        private string uuidRoute;
         private string uuidModel;
-        //private TunnelHandler Handler;
 
         public BasicScene(TunnelHandler Handler)
         {
-            this.CommandList = new Queue<Func<string>>();
+            this.CommandList = new List<Func<string>>();
             this.Handler = Handler;
 
             // Add methods to queue.
-            this.CommandList.Enqueue(CreateTerrain);
-            this.CommandList.Enqueue(RemoveGroundPlane);
-            this.CommandList.Enqueue(ChangeTime);
-            this.CommandList.Enqueue(AddModels);
-            this.CommandList.Enqueue(ChangeTerrainHeight);
-            this.CommandList.Enqueue(AddRoute);
-            this.CommandList.Enqueue(AddRoad);
-            this.CommandList.Enqueue(MoveModelOverRoad);
+            this.CommandList.Add(CreateTerrain);
+            this.CommandList.Add(RemoveGroundPlane);
+            this.CommandList.Add(ChangeTime);
+            this.CommandList.Add(AddModels);
+            this.CommandList.Add(ChangeTerrainHeight);
+            this.CommandList.Add(AddRoute);
+            this.CommandList.Add(AddRoad);
+            this.CommandList.Add(MoveModelOverRoad);
         }
 
         /// <summary>
@@ -36,11 +35,16 @@ namespace TestVREngine
         /// </summary>
         public string ExecuteNext(int index)
         {
-            return this.CommandList.Dequeue().ToString();
+            if (index < this.CommandList.Count) {
+                return this.CommandList[index].ToString();
+            } else
+            {
+                return "There is nothing left to do.";
+            }
         }
 
         /// <summary>
-        /// Step 1.
+        /// Step 1. Create a new terrain with size: 256 x 256.
         /// </summary>
         private string CreateTerrain()
         {
@@ -49,7 +53,7 @@ namespace TestVREngine
         }
 
         /// <summary>
-        /// Step 2.
+        /// Step 2. Remove the terrain.
         /// </summary>
         private string RemoveGroundPlane()
         {
@@ -58,7 +62,7 @@ namespace TestVREngine
         }
 
         /// <summary>
-        /// Step 3.
+        /// Step 3. Change the time to 5:30.
         /// </summary>
         private string ChangeTime()
         {
@@ -67,16 +71,16 @@ namespace TestVREngine
         }
 
         /// <summary>
-        /// Step 4.
+        /// Step 4. Place a new house.
         /// </summary>
         private string AddModels()
         {
-            //this.Handler.SendToTunnel(JSONCommandHelper.Wrap3DObject("house", "data/NetworkEngine/models/houses/set1/house1.obj"), uuidModel);
+            //this.Handler.SendToTunnel(JSONCommandHelper.Wrap3DObject("bike", "data/NetworkEngine/models/bike/bike.fbx", new Transform(1 , new int[3] { 0, 0, 0}, new int[3] { 270, 0, 270 })), uuidModel);
             return "Spawned a house.";
         }
 
         /// <summary>
-        /// Step 5.
+        /// Step 5. Change the terrain height to a linear incline (does not look great)
         /// </summary>
         private string ChangeTerrainHeight()
         {
@@ -87,40 +91,40 @@ namespace TestVREngine
                 terrainHeight[i] = i;
             }
             this.Handler.SendToTunnel(JSONCommandHelper.UpdateTerrainHeight(terrainHeight));*/
-            return "Changed terrain height";
+            return "Changed terrain height.";
         }
 
         /// <summary>
-        /// Step 6.
+        /// Step 6. Create a new route.
         /// </summary>
         private string AddRoute()
         {
             
-            PosVector[] posVectors = new PosVector[];
+            PosVector[] posVectors = new PosVector[7];
             for (int i = 0; i < 6; i++)
             {
                 posVectors[i] = new PosVector(new[] {i, i + 1, i + 2}, new[] {i, i + 1, i + 2});
             }
             //this.Handler.SendToTunnel(JSONCommandHelper.WrapAddRoute(posVectors), uuidRoute);
-            return "";
+            return "Added a route.";
         }
 
         /// <summary>
-        /// Step 7.
+        /// Step 7. Add a road to the previous route.
         /// </summary>
         private string AddRoad()
         { 
             //Handler.SendToTunnel(JSONCommandHelper.WrapAddRouteTerrain(uuidRoute));
-            return "";
+            return "Added a road to the previous route.";
         }
 
         /// <summary>
-        /// Step 8.
+        /// Step 8. Move a model over the route.
         /// </summary>
         private string MoveModelOverRoad()
         {
             //Handler.SendToTunnel(JSONCommandHelper.WrapFollow(uuidRoute,uuidModel));
-            return "";
+            return "The bike is now moving over the route.";
         }
     }
 }
