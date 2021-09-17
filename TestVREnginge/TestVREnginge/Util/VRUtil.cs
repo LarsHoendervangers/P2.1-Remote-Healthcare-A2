979620d41a741eee88248f96d5fd9681959674ce
luuki <lah.vanberkel@student.avans.nl>
2021-09-17 22:18:05 +0200

    Start with render resultution

--- TestVREnginge/TestVREnginge/Util/VRUtil.cs
+++ TestVREnginge/TestVREnginge/Util/VRUtil.cs
@@ -19,24 +19,41 @@ namespace TestVREngine.Util
             return uuID;
         }
 
-        public static float[] GenerateTerrain(int width, int height, int range, float multiplier)
+        public static float[] GenerateTerrain(uint renderresulution, int width, int height, int range, float multiplier)
         {
             Random random = new Random();
-            int randomSeedX = width + random.Next(0, 1000);
-            int randomSeedY = height + random.Next(0, 1000);
+            int renderW = (int)(width * renderresulution);
+            int renderH =(int) (height * renderresulution);
 
-            float[] terrain = new float[width * height];
 
-            for (int i = randomSeedX; i < randomSeedX + width; i++)
+            int randomSeedX = renderW + random.Next(0, 1000);
+            int randomSeedY = renderH + random.Next(0, 1000);
+
+            float[] render = new float[width * height];
+
+            for (int i = randomSeedX; i < randomSeedX + renderW; i++)
             {
-                for (int j = randomSeedY; j < randomSeedY + height; j++)
+                for (int j = randomSeedY; j < randomSeedY + renderH; j++)
                 {
-                    terrain[(i - randomSeedX) * width + (j - randomSeedY)] = SimplexNoise.Noise.CalcPixel2D(i, j, multiplier) / 255f * range;
-
+                    render[(i - randomSeedX) * renderW + (j - randomSeedY)] = SimplexNoise.Noise.CalcPixel2D(i, j, multiplier) / 255f * range;
                 }
             }
 
-       
+            PrintMatirx(render, width);
+
+            float[] terrain = new float[width * height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+              /*      for (int i = 0; i < length; i++)
+                    {
+
+                    }
+                    terraini[i * width + j] = */
+                }
+            }
 
 
 
