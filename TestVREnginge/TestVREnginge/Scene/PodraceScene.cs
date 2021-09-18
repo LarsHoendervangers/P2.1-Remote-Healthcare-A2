439bdadee0d3979b58a61fa7883753cf4af6a54b
Teun Leenders <t.j.tleenders@gmail.com>
2021-09-19 00:36:32 +0200

    Tried to move the path for the podrace to this scene

--- TestVREnginge/TestVREnginge/Scene/PodraceScene.cs
+++ TestVREnginge/TestVREnginge/Scene/PodraceScene.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Diagnostics;
 using TestVREngine.Tunnel;
 using TestVREngine.Util;
@@ -9,9 +10,11 @@ namespace TestVREngine.Scene
 {
     class PodraceScene : GeneralScene
     {
+        private string uuidRoute;
+        private string uuidModel;
+
         public PodraceScene(TunnelHandler handler) : base(handler)
         {
-
         }
 
         public override void InitScene()
@@ -25,13 +28,23 @@ namespace TestVREngine.Scene
         public override void LoadScene()
         {
             //Spawning podracer
-            Handler.SendToTunnel(JSONCommandHelper.Wrap3DObject("podracer", "data/NetworkEngine/models/podracer/podracer.obj"));
+            Handler.SendToTunnel(JSONCommandHelper.Wrap3DObject("podracer", "data/NetworkEngine/models/podracer/podracer.obj"), (string message) => this.uuidModel = VRUTil.GetId(message));
 
             //Spawning map
-            Handler.SendToTunnel(JSONCommandHelper.Wrap3DObject("raceterrain", "data/NetworkEngine/models/podracemap1/podracermap.obj"));
+            //Handler.SendToTunnel(JSONCommandHelper.Wrap3DObject("raceterrain", "data/NetworkEngine/models/podracemap1/podracermap.obj"));
+            Handler.SendToTunnel(JSONCommandHelper.Wrap3DObject("raceterrain", "data/NetworkEngine/models/podracemap1/podracemap1.obj", new Transform(1, new int[3] { 200, -2, 50 }, new int[3] { 0, 0, 0 })));
 
             //Creating terrain
             Debug.WriteLine(CreateTerrain());
+
+            //Adding route
+            AddRoute();
+
+            //Adding road
+            AddRoad();
+
+            //Letting podracer follow the route
+            MoveModelOverRoad();
         }
 
         /// <summary>
@@ -46,6 +59,37 @@ namespace TestVREngine.Scene
             this.Handler.SendToTunnel(JSONCommandHelper.WrapShowTerrain("ground", new Transform(1, new int[3] { -128, 0, -128 }, new int[3] { 0, 0, 0 })));
 
             return "Created a new terrain with size: 256 x 256.";
+        }
+
+        private string AddRoute()
+        {
+            PosVector[] posVectors = new PosVector[]
+            {
+            new PosVector(new int[]{-22,0,40 }, new int[]{5,0,5}),
+            new PosVector(new int[]{0,0,62}, new int[]{5,0,5}),
+            new PosVector(new int[]{42,0, 63}, new int[]{5,0,-5}),
+            new PosVector(new int[]{65,0,42 }, new int[]{5,0,-5}),
+            new PosVector(new int[]{75,0,10 }, new int[]{5,0,-5}),
+            new PosVector(new int[]{63,0,-30 }, new int[]{-5,0,5}),
+            new PosVector(new int[]{20,0,-40 }, new int[]{-5,0,-5}),
+            new PosVector(new int[]{-10,0,-30 }, new int[]{-5,0,5}),
+            new PosVector(new int[]{-25,0,-5 }, new int[]{-5,0,5})
+        };
+
+            Handler.SendToTunnel(JSONCommandHelper.WrapAddRoute(posVectors),(string message) => this.uuidRoute = VRUTil.GetId(message));
+            return "Added a route.";
+        }
+        
+        private string MoveModelOverRoad()
+        {
+            Handler.SendToTunnel(JSONCommandHelper.WrapFollow(uuidRoute, uuidModel));
+            return "The bike is now moving over the route.";
+        }
+
+        private string AddRoad()
+        {
+            Handler.SendToTunnel(JSONCommandHelper.WrapAddRouteTerrain(uuidRoute));
+            return "Added a road to the previous route.";
         }
     }
 }
