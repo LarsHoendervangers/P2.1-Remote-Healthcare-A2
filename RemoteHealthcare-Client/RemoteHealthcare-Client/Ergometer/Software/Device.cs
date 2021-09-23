19aa58b43db397d3b233642908062e97fa97069d
Jesse <jm.krijgsman@student.avans.nl>
2021-09-23 08:44:07 +0200

    Created WPF client application

---
+++ RemoteHealthcare-Client/RemoteHealthcare-Client/Ergometer/Software/Device.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace RemoteHealthcare_Client.Ergometer.Software
+{
+    abstract class Device
+    {
+        //Event handelers for gui that need to implemented.
+        public abstract event EventHandler<double> OnSpeed;
+        public abstract event EventHandler<int> OnRPM;
+        public abstract event EventHandler<int> OnHeartrate;
+        public abstract event EventHandler<double> OnDistance;
+        public abstract event EventHandler<double> OnElapsedTime;
+        public abstract event EventHandler<int> OnTotalPower;
+        public abstract event EventHandler<int> OnCurrentPower;
+
+
+        /// <summary>
+        /// Event that is called by DataGUI when a user enters a resistance value.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="data"></param>
+        public virtual void OnResistanceCall(object sender, int data)
+        {
+            System.Diagnostics.Debug.WriteLine($"Resistance of the bike set to {data}");
+        }
+    }
+}
