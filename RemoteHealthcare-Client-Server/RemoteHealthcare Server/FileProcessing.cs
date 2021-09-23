498e80da95d01d22f6f26361318704f08f3bb0f6
Twan van Noorloos <twan.v.n@outlook.com>
2021-09-23 11:20:18 +0200

    Started on working with the saving and loading of the data using FileIO

--- RemoteHealthcare-Client-Server/RemoteHealthcare Server/FileProcessing.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare Server/FileProcessing.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,14 +8,45 @@ namespace RemoteHealthcare_Server
 {
     public class FileProcessing
     {
-        public void SaveSession()
+        public void SaveSession(Patient p)
         {
-            throw new System.NotImplementedException();
+            string FolderPath = Directory.GetCurrentDirectory() + "/" + p.Username;
+            if (!Directory.Exists(FolderPath))
+            {
+                Directory.CreateDirectory(FolderPath);
+                CreateFile(FolderPath, p.Session);
+            }
+            else
+            {
+                CreateFile(FolderPath, p.Session);
+            }
         }
 
-        public void LoadSession()
+        public JArray LoadSession(Session s)
         {
-            throw new System.NotImplementedException();
+            string FilePath = Directory.GetCurrentDirectory() + "/" + s.Patient.Username + "/" + s.Patient.Username + s.StartTime;
+
+            JArray ReadData = JArray.Parse(File.ReadAllText(FilePath));
+
+            return ReadData;
+        }
+
+        public void CreateFile(string folderpath, Session s)
+        {
+            JArray BikeArray = JArray.FromObject(s.BikeMeasurements);
+            JArray HeartArray = JArray.FromObject(s.HRMeasurements);
+
+            string FilePath = folderpath + "/" + s.Patient.Username + s.StartTime;
+
+            try
+            {
+                File.WriteAllText(FilePath, BikeArray.ToString());
+                File.WriteAllText(FilePath, HeartArray.ToString());
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.Message);
+            }
         }
     }
 }
