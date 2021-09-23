0492de26026abbd185fe37915d9b3193a8874e57
Jesse <jm.krijgsman@student.avans.nl>
2021-09-23 17:45:43 +0200

    Started on startup class

--- RemoteHealthcare-Client/RemoteHealthcare-Client/Startup.cs
+++ RemoteHealthcare-Client/RemoteHealthcare-Client/Startup.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteHealthcare_Client
+{
+    class Startup
+    {
+        private IDataManager VrDataManager;
+        private IDataManager ServerDataManager;
+        private IDataManager DeviceDataManager;
+
+        private string bikeName;
+        private string hrName;
+
+        public Startup()
+        {
+
+        }
+
+        public void setupHandlers()
+        {
+            
+        }
+
+    }
+}
