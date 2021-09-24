409e732a7c3582505ea8698a8ac96749626f737d
luuki <lah.vanberkel@student.avans.nl>
2021-09-24 22:58:45 +0200

    Refactor for jsonreader

--- RemoteHealthcare-Client-Server/RemoteHealthcare Server/Coms/JSONReader.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare Server/Coms/JSONReader.cs
@@ -9,61 +9,117 @@ using System.Text;
 
 namespace RemoteHealthcare_Server
 {
-    public class JSONReader
+    public class JSONReader 
     {
 
         //Switch case for inputs
-        public static void DecodeJsonObject(JObject jObject, EncryptedSender sender)
+        public static void DecodeJsonObject(JObject jObject, EncryptedSender sender, int accessLevel)
         {
             string command = jObject.GetValue("command").ToString();
 
-            switch (command)
+            switch (accessLevel)
             {
-               /* case "login":
-                    LoginAction(jObject, sender);
-                    break;*/
-                case "ergodata":
-                    ReceiveMeasurement(jObject, sender);
+                case 0:
+                    if (command == "ergometer") JSONPatient.ReceiveMeasurement(jObject, sender);
                     break;
+                case 1:
+                    if (command == "abort") JSONDoctor.AbortingClient(jObject, sender);
+                    if (command == "setresist") JSONDoctor.SettingErgometer(jObject, sender);
+                    if (command == "getallclients") JSONDoctor.GetAllClients(jObject, sender);
+                    if (command == "subtopatient") JSONDoctor.SubscribeToLiveSession(jObject, sender);
+                    if (command == "getsession") JSONDoctor.GetHistoricSession(jObject, sender);
+                    if (command == "newsession") JSONDoctor.StartNewSession(jObject, sender);
+                    break;
+
+                case 2:
+                    //Can have all admin features you want
+                    break;
+
             }
         }
 
 
-        //TODO making if there is a database
-  
 
-        //TODO test
-        private static void ReceiveMeasurement(JObject Jobject, EncryptedSender sender)
+        /// <summary>
+        /// This class contains all the methods with acces level 0 so being it patients
+        /// </summary>
+        partial class JSONPatient
         {
-            //Bike
-            JToken rpm = Jobject.SelectToken("data.rpm");
-            JToken speed = Jobject.SelectToken("data.speed");
-            JToken dist = Jobject.SelectToken("data.dist");
-            JToken pow = Jobject.SelectToken("data.pow");
-            JToken accpow = Jobject.SelectToken("data.accpw");
-
-            //Heart
-            JToken bpm = Jobject.SelectToken("data.bpm");
-
-            //All
-            JToken time = Jobject.SelectToken("data.time");
-
-            //Checks
-            if (rpm != null)
+            public static void ReceiveMeasurement(JObject Jobject, EncryptedSender sender)
             {
-               // patient.Session.BikeMeasurements.Add(
-          /*          new BikeMeasurement(DateTime.Parse(time.ToString())
-                    , int.Parse(rpm.ToString()), int.Parse(speed.ToString())
-                    , double.Parse(pow.ToString()), int.Parse(accpow.ToString())
-                    , int.Parse(dist.ToString())));*/
+                //Bike
+                JToken rpm = Jobject.SelectToken("data.rpm");
+                JToken speed = Jobject.SelectToken("data.speed");
+                JToken dist = Jobject.SelectToken("data.dist");
+                JToken pow = Jobject.SelectToken("data.pow");
+                JToken accpow = Jobject.SelectToken("data.accpw");
+
+                //Heart
+                JToken bpm = Jobject.SelectToken("data.bpm");
+
+                //All
+                JToken time = Jobject.SelectToken("data.time");
+
+                //Checks
+                if (rpm != null)
+                {
+                    // patient.Session.BikeMeasurements.Add(
+                    /*          new BikeMeasurement(DateTime.Parse(time.ToString())
+                              , int.Parse(rpm.ToString()), int.Parse(speed.ToString())
+                              , double.Parse(pow.ToString()), int.Parse(accpow.ToString())
+                              , int.Parse(dist.ToString())));*/
+                }
+                else if (bpm != null)
+                {
+                    //  patient.Session.HRMeasurements.Add(new HRMeasurement(
+                    //   DateTime.Parse(time.ToString()), int.Parse(bpm.ToString())));
+                }
             }
-            else if (bpm != null)
+
+          
+        }
+
+        /// <summary>
+        /// This class contains all the methods with acces level 1 so being it doctors
+        /// </summary>
+        partial class JSONDoctor
+        {
+            public static void SettingErgometer(JObject jObject, EncryptedSender sender)
             {
-              //  patient.Session.HRMeasurements.Add(new HRMeasurement(
-                 //   DateTime.Parse(time.ToString()), int.Parse(bpm.ToString())));
+                throw new NotImplementedException();
+            }
+
+            public static void AbortingClient(JObject jObject, EncryptedSender sender)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal static void GetAllClients(JObject jObject, EncryptedSender sender)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal static void SubscribeToLiveSession(JObject jObject, EncryptedSender sender)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal static void GetHistoricSession(JObject jObject, EncryptedSender sender)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal static void StartNewSession(JObject jObject, EncryptedSender sender)
+            {
+                throw new NotImplementedException();
             }
         }
+
+
+
 
 
     }
+
+  
 }
