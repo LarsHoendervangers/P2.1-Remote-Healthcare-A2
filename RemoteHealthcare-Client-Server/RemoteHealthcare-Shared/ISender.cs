68dd3c1e5d7848eca03c805b6e008ed34ab146ec
Lars <larslego@outlook.com>
2021-09-29 10:08:47 +0200

    Added: and implemented ISender

---
+++ RemoteHealthcare-Client-Server/RemoteHealthcare-Shared/ISender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoteHealthcare_Shared
+{
+    interface ISender
+    {
+        string ReadMessage();
+        void SendMessage(string message);
+    }
+}
