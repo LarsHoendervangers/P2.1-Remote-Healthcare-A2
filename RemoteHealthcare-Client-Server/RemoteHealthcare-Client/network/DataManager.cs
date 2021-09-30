2fd0d4f48be80cdab6ab17a2a7f398639c58548b
Jesse <jm.krijgsman@student.avans.nl>
2021-09-30 16:19:30 +0200

    Added documentation for DataManager

--- RemoteHealthcare-Client-Server/RemoteHealthcare-Client/network/DataManager.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare-Client/network/DataManager.cs
@@ -6,17 +6,31 @@ using System.Text;
 
 namespace RemoteHealthcare_Client
 {
+    /// <summary>
+    /// Abstract class that handles the abstration layer above all the different data sources
+    /// </summary>
     public abstract class DataManager
     {
         public IList<DataManager> NetworkManagers { get; set; }
 
+        /// <summary>
+        /// Constructor for the DataManager
+        /// </summary>
         protected DataManager()
         {
             NetworkManagers = new List<DataManager>();
         }
 
+        /// <summary>
+        /// Method that handles the receiving of data from one DataManager to an other
+        /// </summary>
+        /// <param name="data">The data send to the manager</param>
         public abstract void ReceivedData(JObject data);
 
+        /// <summary>
+        /// Sends the given data to all the other dataManagers
+        /// </summary>
+        /// <param name="data">The data to be send</param>
         protected void SendToManagers(JObject data)
         {
             foreach(DataManager manager in NetworkManagers)
