6c919cad823fc51d3326767e776e59f3a7749e29
Twan van Noorloos <twan.v.n@outlook.com>
2021-09-30 13:30:33 +0200

    Created a simple GUI which can switch between 2 views and has corresponding GUI elements

---
+++ RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/ViewModels/PatientViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteHealthcare_Dokter.ViewModels
+{
+    class PatientViewModel
+    {
+        private List<string> _PatientsList;
+        public List<string> PatientList
+        {
+            get { return _PatientsList; }
+            set { _PatientsList = value; }
+        }
+    }
+}
