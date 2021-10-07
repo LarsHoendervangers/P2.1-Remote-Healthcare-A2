ab8d5adf97f8211bf09f8ca9130f809b80b67ccf
Jesse <jm.krijgsman@student.avans.nl>
2021-10-07 13:38:22 +0200

    Fixed the whole branch since teun broke it

--- RemoteHealthcare-Client-Server/RemoteHealthcare-Client/network/ServerDataManager.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare-Client/network/ServerDataManager.cs
@@ -93,7 +93,7 @@ namespace RemoteHealthcare_Client
 
         public override void ReceivedData(JObject data)
         {
-            // The server will only get messages to login, all other is not defined in the data protocol
+            // The server will only get messages to login, all other is not defined in the data protocol.
             Trace.WriteLine($"received data from server: {data}");
             this.TCPClientHandler.WriteMessage(data.ToString());
         }
