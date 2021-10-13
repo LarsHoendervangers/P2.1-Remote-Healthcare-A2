93e06b15b855f39c498549d473c3ee592603e619
Twan van Noorloos <twan.v.n@outlook.com>
2021-10-13 15:36:51 +0200

    Push before starting on live chart

--- RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/ViewModels/SessionDetailViewModel.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/ViewModels/SessionDetailViewModel.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Configurations;
 
 namespace RemoteHealthcare_Dokter.ViewModels
 {
     class SessionDetailViewModel
     {
+        private double _axisMax;
+        private double _axisMin;
+        private double _trend;
+
+        public SessionDetailViewModel() 
+        {
+           
+        }
+
     }
 }
