01a0be11e79e04e45421412b0324e94a046603b6
Twan van Noorloos <twan.v.n@outlook.com>
2021-10-14 14:36:39 +0200

    Updated session window details

--- RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/ViewModels/SessionDetailViewModel.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/ViewModels/SessionDetailViewModel.cs
@@ -21,6 +21,10 @@ namespace RemoteHealthcare_Dokter.ViewModels
             this.Patient = patient;
 
             this.manager = new SessionManager();
+
+            this.FullName = this.Patient.FirstName + " " + this.Patient.LastName;
+            this.Age = "Leeftijd:\t\t" + CalculateAge();
+            this.ID = "ID persoon:\t" + this.Patient.ID;
         }
 
         private SharedPatient _SelectedSessionPatient;
@@ -37,6 +41,57 @@ namespace RemoteHealthcare_Dokter.ViewModels
         private void HandleSessionPatientClicked()
         {
             this.window.Content = new SessionDetailViewModel(this.window, SelectedSessionPatient);
+        }
+
+        private string _FullName;
+        public string FullName
+        {
+            get { return _FullName; }
+            set
+            {
+                _FullName = value;
+            }
+        }
+
+        private string _Age;
+        public string Age
+        {
+            get { return _Age; }
+            set
+            {
+
+                _Age = value;
+            }
+        }
+
+        private int CalculateAge()
+        {
+            int years = DateTime.Now.Year - this.Patient.DateOfBirth.Year;
+            if (DateTime.Now.Month > this.Patient.DateOfBirth.Month)
+            {
+                return years;
+            } else if (DateTime.Now.Month < this.Patient.DateOfBirth.Month)
+            {
+                return years - 1;
+            } else
+            {
+                if (DateTime.Now.Day >= this.Patient.DateOfBirth.Day)
+                {
+                    return years;
+                }
+
+                return years - 1;
+            }
+        }
+
+        private string _ID;
+        public string ID
+        {
+            get { return _ID; }
+            set
+            {
+                _ID = value;
+            }
         }
     }
 }
