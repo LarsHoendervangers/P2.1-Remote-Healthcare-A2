d42973eceb5390112ec1d723c46ecec90da0a2ff
Jesse <jm.krijgsman@student.avans.nl>
2021-10-25 15:01:22 +0200

    Started on getting the historic patients

--- RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/BackEnd/PatientManager.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/BackEnd/PatientManager.cs
@@ -10,38 +10,94 @@ using System.Threading.Tasks;
 
 namespace RemoteHealthcare_Dokter.BackEnd
 {
+    /// <summary>
+    /// Class is a DataManger, so it can send data to the server.
+    /// It handles getting all the patients and patient sessions, used in historic data.
+    /// </summary>
     class PatientManager : DataManager
     {
         public event EventHandler<List<SharedPatient>> OnPatientsReceived;
         public event EventHandler<List<SessionWrap>> OnSessionReceived;
 
+        /// <summary>
+        /// Constructor for the PatientManager.
+        /// </summary>
         public PatientManager()
         {
-
+            // Requesting all patient from the server
+            GetAllPatients();
         }
 
+        /// <summary>
+        /// Method that is called when data is received from the server, is handles the following commands:
+        /// - Patient data comming from the server for all the clients that have had sessions
+        /// - All the saved sessions of a patient.
+        /// The method only checks for these commands, when detected it calles the method to handles that specific command
+        /// </summary>
+        /// <param name="data">The data received</param>
         public override void ReceivedData(JObject data)
         {
-            throw new NotImplementedException();
+            JToken value;
+
+            // Try to get the command tab of the data
+            bool correctCommand = data.TryGetValue("command", StringComparison.InvariantCulture, out value);
+
+            // Return if the parsing of command was not succesfull
+            if (!correctCommand) return;
+
+            switch (value.ToString())
+            {
+                case "getallpatients":
+                    // Setting the command to the command to ask for detailed data and sending to server
+                    data["command"] = "getdetailpatient";
+                    SendToManagers(data);
+                    break;
+                case "detaildata":
+                    HandleIncomingPatients(data);
+                    break;
+                case "getsessions":
+                    // Calling the method that handles the getsessions command
+                    HandleIncomingSessions(data);
+                    break;
+            }
         }
 
-        private void HandleIncoming(JObject data)
-        {
-
-        }
-
-        public List<SharedPatient> GetAllPatients()
-        {
-            List<SharedPatient> patients = new List<SharedPatient>();
-
-            patients.Add(new SharedPatient("Twan", "Van Noorloos", "1", false, DateTime.Now));
-
-            return patients;
-        }
-
+        /// <summary>
+        /// This method handles the converstion for the getsessions command:
+        /// it gets the patientID and names
+        /// </summary>
+        /// <param name="data">The data with the getsessions command</param>
         private void HandleIncomingPatients(JObject data)
         {
+            // Getting the data field on the json object
+            JArray patientIDs = data.GetValue("data") as JArray;
 
+            // looping through all the patients in the json
+            List<SharedPatient> patients = new List<SharedPatient>();
+            foreach (JObject jo in patientIDs)
+            {
+                // Converting the object to a SharedPatient object
+                patients.Add(jo.ToObject<SharedPatient>());
+            }
+
+            // Invoking the event to tell the GUI to update the list
+            this.OnPatientsReceived.Invoke(this, patients);
+        }
+
+        private void HandleIncomingSessions(JObject data)
+        {
+
+        }
+
+        public void GetAllPatients()
+        {
+            // Command to request all clients that are stored in the server, see dataprotocol
+            object o = new
+            {
+                command = "getallpatients",
+            };
+
+            SendToManagers(JObject.FromObject(o));
         }
 
         internal void GetSessions()
