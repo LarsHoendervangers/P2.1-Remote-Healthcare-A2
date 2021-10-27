269add12e2d45e85484dc9e04124a8007a9740e2
Twan van Noorloos <twan.v.n@outlook.com>
2021-10-27 15:55:04 +0200

    Updated session detail manager

--- RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/BackEnd/SessionManager.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/BackEnd/SessionManager.cs
@@ -145,5 +145,24 @@ namespace RemoteHealthcare_Dokter.BackEnd
             // performing all the actions needed when the window is closed
             SubscribeToPatient(this.Patient, false);
         }
+
+        public void StopSession(SharedPatient patient)
+        {
+            string[] patients = new string[] { patient.ID };
+
+            // JSON object to start a new session
+            object o = new
+            {
+                command = "newsession",
+                data = new
+                {
+                    patid = patients,
+                    state = 1
+                }
+            };
+
+            //Asking the sever to start a session, no further actions are taken until new userineraction
+            this.SendToManagers(JObject.FromObject(o));
+        }
     }
 }
