1881d97c4f06db8e8e2cd9922d099e073cc29517
Jesse <jm.krijgsman@student.avans.nl>
2021-10-27 12:08:01 +0200

    Auto stash before merge of "Feature-DokterGUI" and "origin/Feature-DokterGUI"

--- RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/BackEnd/PatientHisoryManager.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare Dokter/BackEnd/PatientHisoryManager.cs
@@ -5,6 +5,7 @@ using RemoteHealthcare_Server;
 using RemoteHealthcare_Shared.DataStructs;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,13 @@ namespace RemoteHealthcare_Dokter.BackEnd
     class PatientHisoryManager : DataManager
     {
         public event EventHandler<Session> OnSessionUpdate;
-
+        SessionWrap session;
+        
         public PatientHisoryManager(SessionWrap session, string userID)
         {
-            GetSessionData(session.Enddate, userID);
+            this.session = session;
+
+            GetSessionData(userID);
         }
 
         public override void ReceivedData(JObject data)
@@ -34,24 +38,62 @@ namespace RemoteHealthcare_Dokter.BackEnd
             {
                 case "getsessionsdetails":
                     // Calling the method that handles the getsessions command
-                    WrapSessionData(data);
+                    HandleIncomingSession(data);
                     break;
             }
         }
 
-        private void HandleIncoming(JObject data)
+        public void GetSessionData(string userID)
         {
+            // Command to request details from a session, see dataprotocol
+            object o = new
+            {
+                command = "getsessionsdetails",
+                data = new
+                {
+                    patid = userID,
+                    date = session.Enddate
+                }
+            };
 
+            SendToManagers(JObject.FromObject(o));
         }
 
-        public void GetSessionData(DateTime dateTime, string userID)
+        private void HandleIncomingSession(JObject data)
         {
+            // Getting the patientID and sesion fields on the json object
+            JToken patientID = data.SelectToken("data.patientid");
+            JObject session = data.SelectToken("data.session") as JObject;
+            
+            // Checking if the fields exist
+            if (patientID == null || session == null) return;
 
-        }
+            //Parsing the measurements
+            JObject startDates = session.SelectToken("startdates") as JObject;
+            JObject endDates = session.SelectToken("enddates") as JObject;
 
-        private void WrapSessionData(JObject data)
-        {
+            JArray hrJson = session.SelectToken("hrdata") as JArray;
+            JArray bikeJson = session.SelectToken("bikedata") as JArray;
 
+            // Returning if the values are null
+            if (startDates == null || endDates == null || hrJson == null || bikeJson == null) return;
+
+            List<HRMeasurement> hRMeasurements = new List<HRMeasurement>();
+            foreach(JObject hr in hrJson)
+            {
+                hRMeasurements.Add(JSONConverter.ConvertHRObject(hr));
+            }
+
+            List<BikeMeasurement> bikeMeasurements = new List<BikeMeasurement>();
+            foreach (JObject bike in bikeJson)
+            {
+                bikeMeasurements.Add(JSONConverter.ConverBikeObject(bike));
+            }
+
+            this.session.BikeMeasurements = bikeMeasurements;
+            this.session.HRMeasurements = hRMeasurements;
+            // Invoking the event to tell the GUI to update the list
+            //this.OnSessionReceived?.Invoke(this, sessions);
         }
     }
 }
