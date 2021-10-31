9558b9f4bd3a1944c67303b278241d20b5070c24
Jesse <jm.krijgsman@Student.avans.nl>
2021-10-31 12:15:42 +0100

    Documented ServerSettings

--- RemoteHealthcare-Client-Server/RemoteHealthcare-Shared/Settings/ServerSettings.cs
+++ RemoteHealthcare-Client-Server/RemoteHealthcare-Shared/Settings/ServerSettings.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace RemoteHealthcare_Shared.Settings
 {
+    /// <summary>
+    /// Class holds the settings that are used to connect to the server
+    /// </summary>
     public static class ServerSettings
     {
 
